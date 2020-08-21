072145ca0159d75d676fab6d161a6ed031446a08
徐明晓 <1206512593@qq.com>
2020-08-21 21:07:33 +0800

    Exercise commit.

--- src/岛屿.java
+++ src/岛屿.java
@@ -10,40 +10,45 @@ class 岛屿 {
         //去行列
         int nr = grid.length;
         int nc = grid[0].length;
-        //行列不能越界，而且不能为“水”
-        if(r < 0 || c < 0 || r >= nr || c >= nc){
-            list.remove(list.size()-1);
+        if(r < 0 || c < 0 || r >= nr || c >= nc||grid[r][c]=='.'){
             return;
         }
-        //置为“水”
         list.add(grid[r][c]);
-        if(list.size()==5&&list.get(1)=='H'&&list.get(2)=='I'&&list.get(3)=='N'&&list.get(4)=='A'){
+        char temp=grid[r][c];
+        grid[r][c]=temp;
+        grid[r][c]='.';
+        if(list.size()==4&&list.get(0)=='H'&&list.get(1)=='I'&&list.get(2)=='N'&&list.get(3)=='A'){
             lists.add(new ArrayList<>(list));
             list.remove(list.size()-1);
             return;
         }
+
         //这个地方是递归，在你寻找一个1的时候就开始深度遍历，直到遇见0，才结束
         dfs(grid, r - 1, c);
         dfs(grid, r + 1, c);
         dfs(grid, r, c - 1);
         dfs(grid, r, c + 1);
+        grid[r][c]=temp;
+
     }
     //主函数
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        //int n = 4;
         char [][] grid=new char[n][n];
         for(int i=0;i<n;i++){
             String S=sc.next();
+            //System.out.print(S);
             for(int j=0;j<n;j++){
                 grid[i][j]=S.charAt(j);
             }
         }
 
         //数组不能为null
-        if(grid == null || grid.length == 0){
-            System.out.print(0);
-        }
+//        if(grid == null || grid.length == 0){
+//
+//        }
         //行，列长度
         int nr = grid.length;
         int nc = grid[0].length;
@@ -60,3 +65,4 @@ class 岛屿 {
         System.out.print(lists.size());
     }
 }
+
