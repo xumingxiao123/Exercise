22b4be338eb89c21a4949b736af6a515c1a4d1eb
徐明晓 <1206512593@qq.com>
2020-08-24 12:17:08 +0800

    Exercise commit.

---
+++ src/进制.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class 进制 {
+
+        public static void main(String[] args) {
+            Scanner cin = new Scanner(System.in);
+            int n=cin.nextInt();
+            for (int i=0;i<n;i++){
+                String s =cin.next();
+                s =cin.next();
+                s =cin.next();
+            }
+            for (int i=0;i<n;i++){
+                System.out.println("1");
+            }
+       }
+}
