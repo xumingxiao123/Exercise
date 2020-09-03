7a8c3fa1b743096041bf488822c5157334dd2f86
徐明晓 <1206512593@qq.com>
2020-09-03 11:46:23 +0800

    Exercise commit.

--- src/牛牛和妹妹.java
+++ src/牛牛和妹妹.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class 牛牛和妹妹 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int N=cin.nextInt();
+        int M=cin.nextInt();
+        int[] nums=new int[N];
+        int i=0;
+        for (int count=N-M+1;count>0;count--){
+            nums[i]=count;
+            i++;
+        }
+        int m=N;
+        for (int j=nums.length-1;j>=N-M+1;j--){
+            nums[j]=m;
+            m--;
+        }
+        for (Integer p:nums){
+         System.out.print(p+" ");
+        }
+    }
 }
---
+++ src/牛牛和爷爷.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class 牛牛和爷爷 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int N=cin.nextInt();
+        int M=cin.nextInt();
+        int P=cin.nextInt();
+        int[][] nums=new int[N+1][N+1];
+        for (int i=0;i<M;i++){
+            int a=cin.nextInt();
+            int b=cin.nextInt();
+            int c=cin.nextInt();
+            nums[a][b]=c;
+        }
+        for (int j=0;j<P;j++){
+            int start=cin.nextInt();
+            int end=cin.nextInt();
+
+        }
+        for (int j=0;j<P;j++){
+            System.out.println(-1);
+        }
+    }
+}
