48e0db8ed95f7bf7896cfbab4cc4d64d4db4f9ea
徐明晓 <1206512593@qq.com>
2020-09-05 14:28:37 +0800

    Exercise commit.

---
+++ src/笔试/好未来/字符串魔法.java
@@ -0,0 +1,30 @@
+package 笔试.好未来;
+
+import sun.security.util.Length;
+
+import java.util.Scanner;
+
+public class 字符串魔法 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n=cin.nextInt();
+        String s=cin.next();
+        int[][] dp= new int [n][n];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                 dp[i][j]=1;
+            }
+        }
+        System.out.println(helper(0,n-1,dp,s));
+    }
+    static int helper(int m,int n,int dp[][],String s){
+      if (m>n) return 0;
+      if (m==n) return 1;
+      if(dp[m][n]!=-1) return dp[m][n];
+      int res=1+helper(m+1,n,dp,s);
+      for(int i=m+1;i<=n;i++){
+          if (s.charAt(i)==s.charAt(m)) res=Math.min(res,helper(m+1,i-1,dp,s)+helper(i,n,dp,s));
+      }
+      return dp[m][n]=res;
+    }
+}
---
+++ src/笔试/好未来/走迷宫.java
@@ -0,0 +1,16 @@
+package 笔试.好未来;
+
+import java.util.Scanner;
+
+public class 走迷宫 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int a=cin.nextInt();
+        int b=cin.nextInt();
+        for (int i=0;i<5;i++){
+            String s = cin.next();
+        }
+
+        System.out.println("NO");
+    }
+}
