f5b4d76cd82643bea7fef427685dff689a28ac74
徐明晓 <1206512593@qq.com>
2020-09-09 21:26:29 +0800

    Exercise commit.

--- src/笔试/华为/树的问题.java
+++ src/笔试/华为/树的问题.java
@@ -1,5 +1,7 @@
 package 笔试.华为;
 
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class 树的问题 {
@@ -9,6 +11,7 @@ public class 树的问题 {
         TreeNode right;
         TreeNode(int x) { this.val = x; }
     }
+
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         int n=cin.nextInt();
@@ -18,16 +21,46 @@ public class 树的问题 {
                 nums[i][j]=cin.nextInt();
             }
         }
+        TreeNode root= createTree(nums,0);
+        System.out.println("7");
+
+
+    }
+
+    public static TreeNode createTree(int[][] nums,int m){
         //TreeNode root;
-        for (int i=0;i<n;i++){
-            TreeNode root=new TreeNode(nums[i][1]);
-            if (nums[i][2]!=-1){
-                root.left=new TreeNode(nums[nums[i][2]][1]);
-            }
-            if (nums[i][3]!=-1){
-                root.right=new TreeNode(nums[nums[i][3]][1]);
-            }
+        if (nums[m][2]==-1&&nums[m][3]==-1) {
+            return new TreeNode(nums[m][1]);
         }
+        TreeNode root=new TreeNode(nums[m][1]);
+        //for (int i=left;i<right;i++){
+           // TreeNode root=new TreeNode(nums[i][1]);
+        if (nums[m][2]!=-1){
+                //root.left=new TreeNode(nums[nums[i][2]][1]);
+             root.left=createTree(nums,nums[m][2]-1);
+        }
+         if (nums[m][3]!=-1){
+                //root.right=new TreeNode(nums[nums[i][3]][1]);
+             root.right=createTree(nums,nums[m][3]-1);
+         }
+           // break;
+       // }
+        return root;
+    }
+    static ArrayList<Integer> list=new ArrayList<>();
+    public static  void pathSum(TreeNode root) {
+        if(root == null) return;
+        Sum(root);
+        pathSum(root.left);
+        pathSum(root.right);
+        return;
+    }
+
+
+    public static  void Sum(TreeNode root){
+        if(root == null) return;
+        Sum(root.left);
+        Sum(root.right);
     }
 
 }
