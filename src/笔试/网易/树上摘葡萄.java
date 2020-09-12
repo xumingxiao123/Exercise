25b3f316d11846f01942c1cb654bdf498304d705
徐明晓 <1206512593@qq.com>
2020-09-12 17:29:34 +0800

    Exercise commit.

--- src/笔试/网易/树上摘葡萄.java
+++ src/笔试/网易/树上摘葡萄.java
@@ -15,14 +15,39 @@ public class 树上摘葡萄 {
         Scanner cin = new Scanner(System.in);
         int n=cin.nextInt();
         int m=cin.nextInt();
+        cin.nextLine();
         ArrayList<String> list=new ArrayList<>();
-        for (int i=0;i<n;i++){
-            list.add(cin.nextLine());
+        ArrayList<Character> list1=new ArrayList<>();
+        //System.out.println(cin.nextLine());
+        for (int i=0;i<m;i++){
+            String s=cin.nextLine();
+            list.add(s);
+            list1.add(s.charAt(0));
         }
-        for (int i=0;i<n;i++){
-           String s=list.get(i);
-           char
+//
+//        System.out.println(list.get(0));
+//        for (int i=0;i<n;i++){
+//            //list1.add(list.get(i).charAt(0));
+//           // System.out.println(list1.get(i));
+//        }
+        int i=0;
+        //for (int i=0;i<n;i++){
+        int count=0;
+
+        while (i<list.size()){
+            String s=list.get(i);
+            char root=s.charAt(0);
+            char left=s.charAt(s.length()-1);
+            if (i+1<=list.size()-1&&root==list.get(i+1).charAt(0)){
+                char right=list.get(i+1).charAt(s.length());
+                if (!list1.contains(left)&&!list1.contains(right)) count++;
+                i++;
+            }
+            i++;
         }
+        System.out.println(count);
+
+       // }
 
     }
 }
