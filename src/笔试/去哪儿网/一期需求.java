00eb1352185aa2031827c2d743dff32719c235df
徐明晓 <1206512593@qq.com>
2020-09-23 21:02:39 +0800

    Exercise commit.

--- src/笔试/去哪儿网/一期需求.java
+++ src/笔试/去哪儿网/一期需求.java
@@ -16,6 +16,18 @@ public class 一期需求 {
         }
         String S1=s1.toString();
         String S2=s2.toString();
-        System.out.print(s1);
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for(int i = 1 ; i < s1.length() + 1 ; i ++){
+            for(int j = 1 ; j < s2.length() + 1 ; j ++){
+                //如果末端相同
+                if(S1.charAt(i-1) == S2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    //如果末端不同
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        System.out.print(dp[s1.length()][s2.length()]);
     }
 }
---
+++ src/笔试/去哪儿网/扑克牌的牌型分析.java
@@ -0,0 +1,7 @@
+package 笔试.去哪儿网;
+
+public class 扑克牌的牌型分析 {
+    public static void main(String[] args) {
+
+    }
+}
